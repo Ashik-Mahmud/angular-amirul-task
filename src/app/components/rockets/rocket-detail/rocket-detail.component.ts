8b415944c2ee1edaec53b1186b4e37e923827ad3
Äshik Mahmud <ashikmamud187@gmail.com>
2023-01-28 22:14:15 +0600

    done to show details of this rocket

--- src/app/components/rockets/rocket-detail/rocket-detail.component.ts
+++ src/app/components/rockets/rocket-detail/rocket-detail.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { RocketsService } from '../../rockets.service';
 
 @Component({
   selector: 'app-rocket-detail',
   templateUrl: './rocket-detail.component.html',
-  styleUrls: ['./rocket-detail.component.scss']
+  styleUrls: ['./rocket-detail.component.scss'],
 })
 export class RocketDetailComponent {
+  rocket: any = {};
+  number: number | string | undefined;
+  isLoading: boolean = false;
 
+  constructor(private router: Router) {}
+
+  // get rocket by flight number
+  async getRocketByFlightNumber() {
+    this.isLoading = true;
+    const data = await fetch(
+      `https://api.spacexdata.com/v3/launches/${this.number}`
+    );
+    const rocket = await data.json();
+    this.isLoading = false;
+    return rocket;
+  }
+
+  onGoBack() {
+    this.router.navigate(['/']);
+  }
+  async ngOnInit() {
+    // get the rocket id from the route
+    const rocketId = this.router.url.split('/')[2];
+    this.number = Number(rocketId);
+    const data = await this.getRocketByFlightNumber();
+    this.rocket = data;
+  }
 }
